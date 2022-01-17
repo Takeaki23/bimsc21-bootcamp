3b674a73dc9b3370fcbb7873ad96b4ef6ffd12d2
Takeaki23 <takeaki.sakakibara@students.iaac.net>
2022-01-17 23:41:00 +0900

    website done

--- session03/script.js
+++ session03/script.js
@@ -1,16 +1,17 @@
-//console.log("hey, I am in an external file")
+//console.log("hey. I am in an external file")
 
-const doc = document.documentElement;
-//console.log(doc)
+const doc = document.documentElement
+//console,log(doc)
+doc.style.fontFamily = "timesnewroman";
+doc.style.background = "lightgrey";
+doc.style.color = "white";
 
-doc.style.fontFamily = "monospace";
-doc.style.background = "lightgrey"
-doc.style.color = "white" 
 
-//access elements from the doc
-const titlebar =  document.querySelector("#child-1");
-//console.log(titlebar.innerHTML);
-titlebar.innerHTML = "This is my website's titlebar" ;
+//accsess elements from the doc
+
+const titlebar = document.documentElement.querySelector("#child-1");
+console.log(titlebar.innnerHTML);
+titlebar.innerHTML = "This is my website's titlebar";
 titlebar.style.fontSize = "20px";
 
 //add elements into the site
@@ -27,31 +28,30 @@ function doSomething(){
 }
 
 const mainText = document.querySelector("p");
-mainText.innerHTML = "" 
+mainText.innerHTML = ""
+
 
 const sidebar = document.querySelector("#child-2");
+
 
 //create a form (user input) to fill the paragraph
 let input = document.createElement("input");
 
 sidebar.appendChild(input);
-sidebar.append()
 
 let submitButton = document.createElement("button");
 submitButton.innerHTML = "Submit";
 sidebar.appendChild(submitButton);
 
-let val = submitButton.addEventListener('click', submitText);
+let val = submitButton.addEventListener("click", submitText);
 
 function submitText(){
     let inputText = input.value;
     console.log(inputText);
     mainText.innerHTML += inputText + " ";
     input.value = "";
+
 };
-
-
-
 
 
 
